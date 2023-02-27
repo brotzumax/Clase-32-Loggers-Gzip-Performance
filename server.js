72e179477c8a6ddf4dccc423143ae7b0b9afc93f
Maximiliano <brotzumaxx@gmail.com>
2023-02-27 02:47:45 -0300

    Nueva dirección bloqueante

--- server.js
+++ server.js
@@ -167,6 +167,33 @@ app.get("/info", (req, res) => {
     );
 });
 
+const processInfo = {
+    platform: process.platform,
+    version: process.version,
+    memoryUsage: process.memoryUsage().rss,
+    execPath: process.execPath,
+    pid: process.pid,
+    cwd: process.cwd(),
+    numCPUs: numCPUs,
+}
+
+app.get("/bloqInfo", (req, res) => {
+    console.log(processInfo);
+    res.send(
+        "<h1>Información de la aplicación</h1>" +
+        "<div style='display: flex; flex-direction: column'>" +
+        "<span>Argumentos de entrada: -p (puerto), -m (modo: FORK - CLUSTER)</span>" +
+        `<span>Nombre de la plataforma: ${process.platform}</span>` +
+        `<span>Versión de node.js: ${process.version}</span>` +
+        `<span>Memoria total reservada (rss): ${process.memoryUsage().rss}</span>` +
+        `<span>Path de ejecución:  ${process.execPath}</span>` +
+        `<span>Process id: ${process.pid}</span>` +
+        `<span>Carpeta del proyecto: ${process.cwd()}</span>` +
+        `<span>Número de procesadores: ${numCPUs}</span>` +
+        "</div>"
+    );
+});
+
 /* app.get("/api/randoms", (req, res) => {
     let cantidadNumeros = Number(req.query.cant);
     const calculo = fork('numerosRandom.js');
