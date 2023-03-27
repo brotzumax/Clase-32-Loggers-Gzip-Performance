5097d0174cce995776b10e6aabac703b63ed7e72
Maximiliano <brotzumaxx@gmail.com>
2023-03-27 17:34:16 -0300

    DTO de productos

---
+++ db/productosDto.js
@@ -0,0 +1,9 @@
+class ProductoDTO {
+    constructor({ title, price, thumbnail }) {
+        this.title = title,
+            this.price = price,
+            this.thumbnail = thumbnail
+    }
+}
+
+export default ProductoDTO;
