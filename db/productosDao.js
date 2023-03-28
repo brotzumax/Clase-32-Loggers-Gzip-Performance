c2aa38296abebfb56c19d534fe424693b75980b7
Maximiliano <brotzumaxx@gmail.com>
2023-03-27 21:01:47 -0300

    Awaits en conexiones SQL

--- db/productosDao.js
+++ db/productosDao.js
@@ -7,7 +7,7 @@ class ProductosDao {
 
     async getAll() {
         try {
-            this.clienteSQL.newKnex();
+            await this.clienteSQL.newKnex();
             const productos = [];
             const rows = await this.clienteSQL.knex.from(this.clienteSQL.tableName).select("*");
             for (let row of rows) {
@@ -17,43 +17,43 @@ class ProductosDao {
         } catch (error) {
             console.log(error);
         } finally {
-            this.clienteSQL.destroyKnex();
+            await this.clienteSQL.destroyKnex();
         }
     }
 
     async add(producto) {
         try {
-            this.clienteSQL.newKnex();
+            await this.clienteSQL.newKnex();
             await this.clienteSQL.knex(this.clienteSQL.tableName).insert(producto);
             console.log("Producto añadido con exito");
         } catch (error) {
             console.log(error);
         } finally {
-            this.clienteSQL.destroyKnex();
+            await this.clienteSQL.destroyKnex();
         }
     }
 
     async update(id, productoActualizado) {
         try {
-            this.clienteSQL.newKnex();
+            await this.clienteSQL.newKnex();
             await this.clienteSQL.knex(this.clienteSQL.tableName).where({ id }).update(productoActualizado);
             console.log("Producto actualizado con éxito");
         } catch (error) {
             console.log(error);
         } finally {
-            this.clienteSQL.destroyKnex();
+            await this.clienteSQL.destroyKnex();
         }
     }
 
     async delete(id) {
         try {
-            this.clienteSQL.newKnex();
+            await this.clienteSQL.newKnex();
             await this.clienteSQL.knex(this.clienteSQL.tableName).where({ id }).del();
             console.log("Producto eliminado con éxito");
         } catch (error) {
             console.log(error);
         } finally {
-            this.clienteSQL.destroyKnex();
+            await this.clienteSQL.destroyKnex();
         }
     }
 }
